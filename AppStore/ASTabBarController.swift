81c0bf8311ae2f6878e327e4b31504bf81b8bc2d
mancmiller <manc.miller@gmail.com>
2023-04-10 16:57:44 +0600

    Created a custom TabBarController

---
+++ AppStore/ASTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarController.swift
+//  AppStore
+//
+//  Created by Muslim on 10.04.2023.
+//
+
+import UIKit
+
+class ASTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
